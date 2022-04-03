65b27abbb5ba028ed9affd787fa0e4f3e31e418b
Tom Bigelajzen <tombigel@wix.com>
2022-04-03 16:36:48 +0300

    cleanup

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ function formToURL(form) {
 
 function URLToForm(form) {
   const urlParams = new URLSearchParams(window.location.search);
-  const checkboxGroups = {};
   const elements = [...form.elements];
 
   // Iterate over all form elements that are referenced in the url search params
@@ -26,28 +25,6 @@ function URLToForm(form) {
     }
     element.value = urlParams.get(element.name)
   }
-
-
-
-  // for (const [name, value] of urlParams.entries()) {
-  //   if (form.elements[name]) {
-  //     // checkboxes are special, they don't have a value in form.elements, and they are represented with a repeating key in the url
-  //     // Also, if there is one checkbox in a group =there is no group, so we need to account for a single one too.
-  //     // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/checkbox#handling_multiple_checkboxes
-  //     if (form.elements[name].type === 'checkbox' || form.elements[name][0]?.type === 'checkbox') {
-  //       checkboxGroups[name] ? checkboxGroups[name].push(value) : checkboxGroups[name] = [value];
-  //     }
-  //     // Set value to the saved value
-  //     form.elements[name].value = value;
-  //   }
-  // }
-
-  // // Now handle checkboxes
-  // for (const [name, values] of Object.entries(checkboxGroups)) {
-  //   form.elements[name].type === 'checkbox'
-  //     ? form.elements[name].checked = values.includes(form.elements[name].value) 
-  //     : form.elements[name].forEach(element => element.checked = values.includes(element.value));
-  // }
 }
 
 function URLToClipboard() {
