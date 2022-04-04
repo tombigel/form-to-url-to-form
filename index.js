2fb568b1e761af8814ed90555b36d35197a5c6dd
Tom Bigelajzen <tombigel@wix.com>
2022-04-05 00:04:36 +0300

    quotes, editorcofig

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
  * @param {HTMLFormElement} form
  */
 export function formToURL(form) {
-  const formData = new FormData(form);
+    const formData = new FormData(form);
 
-  // Update URL without reloading
-  if (history.pushState) {
-    const url = new URL(window.location.href);
-    url.search = new URLSearchParams(formData).toString();
-    window.history.pushState({ path: url.href }, "", url.href);
-  }
+    // Update URL without reloading
+    if (history.pushState) {
+        const url = new URL(window.location.href);
+        url.search = new URLSearchParams(formData).toString();
+        window.history.pushState({ path: url.href }, '', url.href);
+    }
 }
 
 /**
@@ -18,45 +18,42 @@ export function formToURL(form) {
  * @param {HTMLFormElement} form
  */
 export function URLToForm(form) {
-  const urlParams = new URLSearchParams(window.location.search);
-  const elements = [...form.elements];
+    const urlParams = new URLSearchParams(window.location.search);
+    const elements = [...form.elements];
 
-  // If no url params, reset the form to its defaults
-  if ([...urlParams.entries()].length === 0) {
-    form.reset();
-    return;
-  }
-  // Iterate over all form elements that are referenced in the url search params
-  for (const element of elements) {
-    // checkboxes, radios and multiselect selects are special, they are not set with value but with checked/selected,
-    // and they might multiple key representations in the url
-    // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/checkbox#handling_multiple_checkboxes
-    if (urlParams.has(element.name)) {
-      // Handle checkboxes and radios
-      if (element.type === "checkbox" || element.type === "radio") {
-        const values = urlParams.getAll(element.name);
-        element.checked = values.includes(element.value);
-      }
-      // Handle multiselect
-      else if (element.type === "select-multiple") {
-        const values = urlParams.getAll(element.name);
-        [...element.querySelectorAll("option")].map(
-          (option) => (option.selected = values.includes(option.value))
-        );
-      }
-      // Handle all other form elements
-      else {
-        element.value = urlParams.get(element.name);
-      }
+    // If no url params, reset the form to its defaults
+    if ([...urlParams.entries()].length === 0) {
+        form.reset();
+        return;
     }
-    // Radios and checkboxes which are not on the list and are checked by default should be deselected
-    else if (
-      (element.type === "checkbox" || element.type === "radio") &&
-      element.checked
-    ) {
-      element.checked = false;
+    // Iterate over all form elements that are referenced in the url search params
+    for (const element of elements) {
+        // checkboxes, radios and multiselect selects are special, they are not set with value but with checked/selected,
+        // and they might multiple key representations in the url
+        // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/checkbox#handling_multiple_checkboxes
+        if (urlParams.has(element.name)) {
+            // Handle checkboxes and radios
+            if (element.type === 'checkbox' || element.type === 'radio') {
+                const values = urlParams.getAll(element.name);
+                element.checked = values.includes(element.value);
+            }
+            // Handle multiselect
+            else if (element.type === 'select-multiple') {
+                const values = urlParams.getAll(element.name);
+                [...element.querySelectorAll('option')].map(
+                    (option) => (option.selected = values.includes(option.value))
+                );
+            }
+            // Handle all other form elements
+            else {
+                element.value = urlParams.get(element.name);
+            }
+        }
+        // Radios and checkboxes which are not on the list and are checked by default should be deselected
+        else if ((element.type === 'checkbox' || element.type === 'radio') && element.checked) {
+            element.checked = false;
+        }
     }
-  }
 }
 
 /**
@@ -65,8 +62,6 @@ export function URLToForm(form) {
  * @returns {Promise<void>}
  */
 export function URLToClipboard(searchOnly) {
-  // Returns a Promise
-  return navigator.clipboard.writeText(
-    searchOnly ? location.search : location.href
-  );
+    // Returns a Promise
+    return navigator.clipboard.writeText(searchOnly ? location.search : location.href);
 }
--- script.js
+++ script.js
@@ -1,50 +1,50 @@
 // polyfill for form.requestSubmit in Safari, should be removed when the feature is enabled https://bugs.webkit.org/show_bug.cgi?id=197958
-import formRequestSubmitPolyfill from "https://cdn.skypack.dev/pin/form-request-submit-polyfill@v2.0.0-szOipIemxchOslzcqvLN/mode=imports,min/optimized/form-request-submit-polyfill.js";
-import { URLToForm, formToURL, URLToClipboard } from './index.js'
+import formRequestSubmitPolyfill from 'https://cdn.skypack.dev/pin/form-request-submit-polyfill@v2.0.0-szOipIemxchOslzcqvLN/mode=imports,min/optimized/form-request-submit-polyfill.js';
+import { URLToForm, formToURL, URLToClipboard } from './index.js';
 
 function init() {
-  const form = document.forms[0];
-  const link = document.getElementById("url-to-clipboard");
-  const submitter = document.getElementById("submitter");
+    const form = document.forms[0];
+    const link = document.getElementById('url-to-clipboard');
+    const submitter = document.getElementById('submitter');
 
-  // Update form from url params
-  URLToForm(form);
+    // Update form from url params
+    URLToForm(form);
 
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
 
-    if (window !== window.top) {
-      document.getElementById("no-form-submit").show();
-      return;
-    }
-    // Update url params from form
-    formToURL(form);
-  });
-
-  link.addEventListener("click", (event) => {
-    event.preventDefault();
-
-    if (window !== window.top) {
-      document.getElementById("no-form-submit").show();
-      reutrn;
-    }
-
-    URLToClipboard().then(() => {
-      event.target.querySelector("span").textContent = "(Copied!)";
+        if (window !== window.top) {
+            document.getElementById('no-form-submit').show();
+            return;
+        }
+        // Update url params from form
+        formToURL(form);
     });
-  });
 
-  submitter.addEventListener("click", (event) => {
-    event.preventDefault();
-    form.requestSubmit();
-  });
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        if (window !== window.top) {
+            document.getElementById('no-form-submit').show();
+            reutrn;
+        }
+
+        URLToClipboard().then(() => {
+            event.target.querySelector('span').textContent = '(Copied!)';
+        });
+    });
+
+    submitter.addEventListener('click', (event) => {
+        event.preventDefault();
+        form.requestSubmit();
+    });
 }
 
 /**
  * Not really necesary, but reminds me of the good ol' days.
  */
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", init);
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init);
 } else {
-  init();
+    init();
 }
