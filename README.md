0a4ccc357f1fdc4281e0f5e9afe17557f522ba45
Tom Bigelajzen <tombigel@wix.com>
2022-04-04 23:16:20 +0300

    comments and readme

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
 # Save and Load Form state to and from the URL
 
-Forms are a great way to save state, their data is serializable and is fairly easy to deserialize it back to the form.  
+## API
+
+```formToURL(form: HTMLFormElement): void```
+Serialize form data to a search params string and push it to the URL without reloading the page (using the history API)
+
+```URLToForm(form: HTMLFormElement): void```
+Parse the search params and set form values
+
+```URLToClipboard(searchOnly?: boolean): Promise```
+Write current url to clipboard
+`searchOnly`: copy only the search parmas part of the url
+
+## Demo
+
+<https://tombigel.github.io/form-to-url-to-form/>
+
+Forms are a great way to manage state for UI, their data is serializable and is fairly easy to deserialize it back to the form.  
 We can push the entire form state to the url and create a savable state with very little code and no framework dependency.  
 This demo shows a way to do it using `FormData`, `URLSearchParams`, `form.requestSubmit()` and `history.pushState`.  
 
 The only external dependency is a polyfill for `form.requestSubmit()`.  
 Safari's implementation is behind a flag for about a year now and there is no info on if and when it will be open by default (<https://bugs.webkit.org/show_bug.cgi?id=197958>).
+
+
 
 I created this as a Pen on CodePen.io initially. Original URL: [https://codepen.io/tombigel/pen/rNpYJKX](https://codepen.io/tombigel/pen/rNpYJKX) but this code changes the URL search params so it can't work inside an iframe that has no control over the top window URL.  
 I'm Keeping the code also in Codepen but for the demo purposes we need Github Pages.
--- index.js
+++ index.js
@@ -1,58 +1,72 @@
+/**
+ * Serialize form data to a search params string and push it to the URL without reloading the page (using the history API)
+ * @param {HTMLFormElement} form
+ */
 export function formToURL(form) {
-    const formData = new FormData(form);
-  
-    // Update URL without reloading
-    if (history.pushState) {
-      const url = new URL(window.location.href);
-      url.search = new URLSearchParams(formData).toString();
-      window.history.pushState({ path: url.href }, "", url.href);
-    }
+  const formData = new FormData(form);
+
+  // Update URL without reloading
+  if (history.pushState) {
+    const url = new URL(window.location.href);
+    url.search = new URLSearchParams(formData).toString();
+    window.history.pushState({ path: url.href }, "", url.href);
   }
-  
- export function URLToForm(form) {
-    const urlParams = new URLSearchParams(window.location.search);
-    const elements = [...form.elements];
-  
-    // If no url params, reset the form to its defaults
-    if ([...urlParams.entries()].length === 0) {
-      form.reset();
-      return;
-    }
-    // Iterate over all form elements that are referenced in the url search params
-    for (const element of elements) {
-      // checkboxes, radios and multiselect selects are special, they are not set with value but with checked/selected,
-      // and they might multiple key representations in the url
-      // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/checkbox#handling_multiple_checkboxes
-      if (urlParams.has(element.name)) {
-        // Handle checkboxes and radios
-        if (element.type === "checkbox" || element.type === "radio") {
-          const values = urlParams.getAll(element.name);
-          element.checked = values.includes(element.value);
-        } 
-        // Handle multiselect
-        else if (element.type === "select" && element.multiple) {
-          const values = urlParams.getAll(element.name);
-          [...element.querySelectorAll("option")].map(
-            (option) => (option.selected = values.includes(option.value))
-          );
-        } 
-        // Handle all other form elements
-        else {
-          element.value = urlParams.get(element.name);
-        }
-      } 
-      // Radios and checkboxes which are not on the list and are checked by default should be deselected
-      else if (
-        (element.type === "checkbox" || element.type === "radio") &&
-        element.checked
-      ) {
-        element.checked = false;
+}
+
+/**
+ * Parse the search params and set form values
+ * @param {HTMLFormElement} form
+ */
+export function URLToForm(form) {
+  const urlParams = new URLSearchParams(window.location.search);
+  const elements = [...form.elements];
+
+  // If no url params, reset the form to its defaults
+  if ([...urlParams.entries()].length === 0) {
+    form.reset();
+    return;
+  }
+  // Iterate over all form elements that are referenced in the url search params
+  for (const element of elements) {
+    // checkboxes, radios and multiselect selects are special, they are not set with value but with checked/selected,
+    // and they might multiple key representations in the url
+    // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/checkbox#handling_multiple_checkboxes
+    if (urlParams.has(element.name)) {
+      // Handle checkboxes and radios
+      if (element.type === "checkbox" || element.type === "radio") {
+        const values = urlParams.getAll(element.name);
+        element.checked = values.includes(element.value);
+      }
+      // Handle multiselect
+      else if (element.type === "select" && element.multiple) {
+        const values = urlParams.getAll(element.name);
+        [...element.querySelectorAll("option")].map(
+          (option) => (option.selected = values.includes(option.value))
+        );
+      }
+      // Handle all other form elements
+      else {
+        element.value = urlParams.get(element.name);
       }
     }
+    // Radios and checkboxes which are not on the list and are checked by default should be deselected
+    else if (
+      (element.type === "checkbox" || element.type === "radio") &&
+      element.checked
+    ) {
+      element.checked = false;
+    }
   }
-  
- export function URLToClipboard() {
-    // Returns a Promise
-    return navigator.clipboard.writeText(location.href);
-  }
-  
+}
+
+/**
+ * Write current url to clipboard
+ * @param {boolean?} searchOnly copy only the search parmas part of the url
+ * @returns {Promise}
+ */
+export function URLToClipboard(searchOnly) {
+  // Returns a Promise
+  return navigator.clipboard.writeText(
+    searchOnly ? location.search : location.href
+  );
+}
