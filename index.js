e7ff9d2cff28b9bce67a67852f2cef4ca6705e4e
Tom Bigelajzen <tombigel@wix.com>
2022-04-04 23:04:39 +0300

    seperated functions to index.js

---
+++ index.js
@@ -0,0 +1,58 @@
+export function formToURL(form) {
+    const formData = new FormData(form);
+  
+    // Update URL without reloading
+    if (history.pushState) {
+      const url = new URL(window.location.href);
+      url.search = new URLSearchParams(formData).toString();
+      window.history.pushState({ path: url.href }, "", url.href);
+    }
+  }
+  
+ export function URLToForm(form) {
+    const urlParams = new URLSearchParams(window.location.search);
+    const elements = [...form.elements];
+  
+    // If no url params, reset the form to its defaults
+    if ([...urlParams.entries()].length === 0) {
+      form.reset();
+      return;
+    }
+    // Iterate over all form elements that are referenced in the url search params
+    for (const element of elements) {
+      // checkboxes, radios and multiselect selects are special, they are not set with value but with checked/selected,
+      // and they might multiple key representations in the url
+      // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/checkbox#handling_multiple_checkboxes
+      if (urlParams.has(element.name)) {
+        // Handle checkboxes and radios
+        if (element.type === "checkbox" || element.type === "radio") {
+          const values = urlParams.getAll(element.name);
+          element.checked = values.includes(element.value);
+        } 
+        // Handle multiselect
+        else if (element.type === "select" && element.multiple) {
+          const values = urlParams.getAll(element.name);
+          [...element.querySelectorAll("option")].map(
+            (option) => (option.selected = values.includes(option.value))
+          );
+        } 
+        // Handle all other form elements
+        else {
+          element.value = urlParams.get(element.name);
+        }
+      } 
+      // Radios and checkboxes which are not on the list and are checked by default should be deselected
+      else if (
+        (element.type === "checkbox" || element.type === "radio") &&
+        element.checked
+      ) {
+        element.checked = false;
+      }
+    }
+  }
+  
+ export function URLToClipboard() {
+    // Returns a Promise
+    return navigator.clipboard.writeText(location.href);
+  }
+  
--- script.js
+++ script.js
@@ -1,63 +1,6 @@
 // polyfill for form.requestSubmit in Safari, should be removed when the feature is enabled https://bugs.webkit.org/show_bug.cgi?id=197958
 import formRequestSubmitPolyfill from "https://cdn.skypack.dev/pin/form-request-submit-polyfill@v2.0.0-szOipIemxchOslzcqvLN/mode=imports,min/optimized/form-request-submit-polyfill.js";
-
-function formToURL(form) {
-  const formData = new FormData(form);
-
-  // Update URL without reloading
-  if (history.pushState) {
-    const url = new URL(window.location.href);
-    url.search = new URLSearchParams(formData).toString();
-    window.history.pushState({ path: url.href }, "", url.href);
-  }
-}
-
-function URLToForm(form) {
-  const urlParams = new URLSearchParams(window.location.search);
-  const elements = [...form.elements];
-
-  // If no url params, reset the form to its defaults
-  if ([...urlParams.entries()].length === 0) {
-    form.reset();
-    return;
-  }
-  // Iterate over all form elements that are referenced in the url search params
-  for (const element of elements) {
-    // checkboxes, radios and multiselect selects are special, they are not set with value but with checked/selected,
-    // and they might multiple key representations in the url
-    // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/checkbox#handling_multiple_checkboxes
-    if (urlParams.has(element.name)) {
-      // Handle checkboxes and radios
-      if (element.type === "checkbox" || element.type === "radio") {
-        const values = urlParams.getAll(element.name);
-        element.checked = values.includes(element.value);
-      } 
-      // Handle multiselect
-      else if (element.type === "select" && element.multiple) {
-        const values = urlParams.getAll(element.name);
-        [...element.querySelectorAll("option")].map(
-          (option) => (option.selected = values.includes(option.value))
-        );
-      } 
-      // Handle all other form elements
-      else {
-        element.value = urlParams.get(element.name);
-      }
-    } 
-    // Radios and checkboxes which are not on the list and are checked by default should be deselected
-    else if (
-      (element.type === "checkbox" || element.type === "radio") &&
-      element.checked
-    ) {
-      element.checked = false;
-    }
-  }
-}
-
-function URLToClipboard() {
-  // Returns a Promise
-  return navigator.clipboard.writeText(location.href);
-}
+import { URLToForm, formToURL, URLToClipboard } from './index.js'
 
 function init() {
   const form = document.forms[0];
